2def63e6913382ce88a0cfbaf7558af1b0849227
RitaFaddeva <cool.attainment@yandex.ru>
2017-05-28 13:11:00 +0300

    Add files via upload

---
+++ geometric.h
@@ -0,0 +1,22 @@
+#ifndef GEOMETRIC_H
+#define GEOMETRIC_H
+
+
+class Geometric
+{
+public:
+    Geometric(int *xp, int *yp, int *rpmp, int *mxrpm, int *st);
+    ~Geometric();
+    void process(int depth);    // демонстрационная процедура обработки детали. параметр - в микрометрах
+private:
+    int *x;                     // координата X сверла
+    int *y;                     // координата Y сверла
+    int *rpm;                   // текущее число оборотов в минуту
+    int *maxrpm;                // максимальное число оборотов в минуту
+    int micmpr;                 // число проходимых сверлом микрометров за оборот
+    int drill;                  // диаметр сверла в мм
+    int *state;                 // переменная состояния. 0 - станок в режиме ожидания,
+                                // 1 - в процессе обработки, иначе - код ошибки
+};
+
+#endif // GEOMETRIC_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "machine.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Machine m;
+    m.process(1000);
+    return 0;
+}
